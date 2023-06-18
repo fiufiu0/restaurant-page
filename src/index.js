2e5f3c2be5c598d191c25832d6aef8f7e2471221
lukaszgromadzki <28166634+lukaszgromadzki@users.noreply.github.com>
2023-06-18 16:46:00 +0200

    contact.js content

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { home } from "./tabs/home";
 import "./style.css";
 import { menu } from "./tabs/menu";
+import { contact } from "./tabs/contact";
 console.log("test");
 
 // home();
-menu();
+// menu();
+contact();
 
 // <div id="content">
 //       <div class="header">
--- src/tabs/contact.js
+++ src/tabs/contact.js
@@ -2,9 +2,31 @@ import { footer } from "../footer";
 import { initialPage } from "../initial";
 
 function contact() {
-    initialPage()
+  initialPage();
 
-    
+  const content = document.getElementById("content");
+  const contact = document.createElement("div");
+  const contactInfo = document.createElement("div");
+  const contactHours = document.createElement("div");
+  const email = document.createElement("p");
+  const adress = document.createElement("p");
+  const phone = document.createElement("p");
+  const weekdays = document.createElement("p");
+  const weekend = document.createElement("p");
 
-    footer();
+  email.textContent = "lemon@smoothie.com";
+  adress.textContent = "62 Madison Ave, New York, NY 10016";
+  phone.textContent = "+1 234-456-7890";
+  weekdays.textContent = "Monday - Friday: 8am - 7pm";
+  weekend.textContent = "Saturday - Sunday: 9am - 7pm";
+
+  contactInfo.append(email, adress, phone)
+  contactHours.append(weekdays,weekend);
+  contact.append(contactInfo,contactHours);
+
+  content.appendChild(contact)
+
+  footer();
 }
+
+export { contact };
